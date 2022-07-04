db373d35a5c2b6dda256b583e32bae5e55f0de96
Мария Одринская <91982304+mariao-max@users.noreply.github.com>
2022-07-04 17:33:49 +0300

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='12345')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", 'localhost', '84.252.142.228']
+ALLOWED_HOSTS = ["*", 'localhost', '84.252.142.228', 'yourfoodgram.ddns.net']
 
 AUTH_USER_MODEL = 'users.User'
 
